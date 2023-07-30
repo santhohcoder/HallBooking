5a48f7bbaf2239acb6b13d585c33adab673aea12
SANTHOSH KANNA <praddossanthosh@gmail.com>
2023-07-30 13:46:54 +0530

    intial

--- test.js
+++
@@ -1,3 +0,0 @@
-console.log(new Date().toLocaleDateString());
-console.log(new Date('July 1, 2022, 9:00:00').toLocaleTimeString());
-console.log(new Date('July 1, 2022, 13:00:00').toLocaleTimeString());
